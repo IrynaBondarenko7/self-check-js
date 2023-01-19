0847d69f2c2993529ff1cdf7721d6ff49c4ee550
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-01-19 19:44:13 +0000

    added method find

---
+++ js/find_method.js
@@ -0,0 +1,35 @@
+//1)У змінній bookWithTitle утворився об'єкт книги, назва якої (властивість title)
+//збігається зі значенням змінної BOOK_TITLE.
+//У змінній bookByAuthor утворився об'єкт книги,
+//автор якої (властивість author) збігається зі значенням змінної AUTHOR
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+// ];
+// const BOOK_TITLE = "The Dream of a Ridiculous Man";
+// const AUTHOR = "Robert Sheckley";
+// // Change code below this line
+
+// const bookWithTitle = books.find((book) => book.title === BOOK_TITLE);
+// const bookByAuthor = books.find((book) => book.author === AUTHOR);
+
+//2)щоб вона повертала об'єкт користувача,
+//пошта якого (властивість email) збігається зі значенням параметра email.
+
+// const getUserWithEmail = (users, email) =>
+//   users.find((user) => user.email === email);
