87d4bf30bdff1feaec348c5c23b97dd90dd0aa38
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-01-30 20:54:55 +0000

    added classes

--- js/hw.js
+++ js/hw.js
@@ -69,3 +69,46 @@
 // console.log(caclculateAverage(25, 15, 64));
 // // ...args=5,10;
 // // []=...args
+
+//Напишіть дві функції
+// letMeSeeYourName(callback) - запитує ім'я користувача
+//через prompt та викликає callback функцію
+//greet(name) - коллбек, що приймає ім'я і логірує в консоль
+//Рядок "Привіт <name>"
+
+// const letMeSeeYourName = (callback) => {
+//   const result = prompt("Введіть ваше ім'я");
+//   callback(result);
+// };
+// const greet = (name) => {
+//   console.log(`Привіт ${name}`);
+// };
+
+// letMeSeeYourName(greet);
+
+//Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
+
+// const makeProduct = (name, price, callback) => {
+//   const product = {
+//     name,
+//     price,
+//     id: Math.random(),
+//   };
+//   callback(product);
+// };
+// const showProduct = (product) => {
+//   console.log(product);
+// };
+// makeProduct("iphone", 5000, showProduct);
+
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+// console.log(myStr); //кожний рядок починається з нового рядка
+
+
