a342d0fd37094ac6a019d1b6873651b532065f73
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-02-13 20:48:06 +0000

    added tasks

--- js/freecodecamp.js
+++ js/freecodecamp.js
@@ -1,42 +1,44 @@
-const recordCollection = {
-  2548: {
-    albumTitle: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: ["Let It Rock", "You Give Love a Bad Name"],
-  },
-  2468: {
-    albumTitle: "1999",
-    artist: "Prince",
-    tracks: ["1999", "Little Red Corvette"],
-  },
-  1245: {
-    artist: "Robert Palmer",
-    tracks: [],
-  },
-  5439: {
-    albumTitle: "ABBA Gold",
-  },
-};
+// task 1 Record Collection
 
-//перший варіант не працює records[id].hasOwnPropery("tracks")
+//const recordCollection = {
+//   2548: {
+//     albumTitle: "Slippery When Wet",
+//     artist: "Bon Jovi",
+//     tracks: ["Let It Rock", "You Give Love a Bad Name"],
+//   },
+//   2468: {
+//     albumTitle: "1999",
+//     artist: "Prince",
+//     tracks: ["1999", "Little Red Corvette"],
+//   },
+//   1245: {
+//     artist: "Robert Palmer",
+//     tracks: [],
+//   },
+//   5439: {
+//     albumTitle: "ABBA Gold",
+//   },
+// };
 
-function updateRecords(records, id, prop, value) {
-  if (prop !== "tracks" && value !== "") {
-    records[id][prop] = value;
-  } else if (
-    prop === "tracks" &&
-    records[id].hasOwnPropery("tracks") === false
-  ) {
-    records[id][prop] = [value];
-  } else if (prop === "tracks" && value !== "") {
-    records[id][prop].push(value);
-  } else if (value === "") {
-    delete records[id][prop];
-  }
-  return records;
-}
+//перший варіант
 
-//  другий варіант працює
+// function updateRecords(records, id, prop, value) {
+//   if (prop !== "tracks" && value !== "") {
+//     records[id][prop] = value;
+//   } else if (
+//     prop === "tracks" &&
+//     records[id].hasOwnProperty("tracks") === false
+//   ) {
+//     records[id][prop] = [value];
+//   } else if (prop === "tracks" && value !== "") {
+//     records[id][prop].push(value);
+//   } else if (value === "") {
+//     delete records[id][prop];
+//   }
+//   return records;
+// }
+
+//  другий варіант
 
 // function updateRecords(records, id, prop, value) {
 //   if (prop !== "tracks" && value !== "") {
@@ -62,7 +64,7 @@ function updateRecords(records, id, prop, value) {
 // console.log(
 //   updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
 // ); //tracks should have the string Addicted to Love as the last element.
-console.log(updateRecords(recordCollection, 2468, "tracks", "Free")); //tracks should have the string 1999 as the first element.
+// console.log(updateRecords(recordCollection, 2468, "tracks", "Free")); //tracks should have the string 1999 as the first element.
 //pdateRecords(recordCollection, 2468, "tracks", "Free"), //tracks tracks should not be set
 
 // console.log(
@@ -74,3 +76,47 @@ console.log(updateRecords(recordCollection, 2468, "tracks", "Free")); //tracks s
 // );
 
 // console.log(updateRecords(recordCollection, 2548, "tracks", ""));
+
+// task 2
+// const myArray = [];
+
+// // Only change code below this line
+// let i = 0;
+// while (i < 6) {
+//   myArray.unshift(i);
+//   i += 1;
+// }
+// console.log(myArray);
+
+//task 3 Iterate Through an Array with a For Loop
+
+// const myArr = [2, 3, 4, 5, 6];
+
+// // Only change code below this line
+// let total = 0;
+// for (let i = 0; i < myArr.length; i += 1) {
+//   total += myArr[i];
+// }
+// console.log(total);
+
+//task 4 Nesting For Loops
+
+// function multiplyAll(arr) {
+//   let product = 1;
+//   // Only change code below this line
+//   for (let i = 0; i < arr.length; i += 1) {
+//     for (let j = 0; j < arr[i].length; j += 1) {
+//       product *= arr[i][j];
+//     }
+//   }
+//   // Only change code above this line
+//   return product;
+// }
+
+// console.log(
+//   multiplyAll([
+//     [1, 2],
+//     [3, 4],
+//     [5, 6, 7],
+//   ])
+// );
